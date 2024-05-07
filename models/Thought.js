72b0b1f068384ff48700518da0818835b83962f9
Dee Cee <153252185+dcho10@users.noreply.github.com>
2024-05-07 19:33:27 -0400

    Completed models

---
+++ models/Thought.js
@@ -0,0 +1,46 @@
+const { Schema, model, Types } = require("mongoose");
+
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            text: String,
+            required: true,
+            min_length: 1,
+            max_length: 280,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now(),
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        reactions: [reactionSchema],
+    }
+);
+
+thoughtSchema.virtual("reactionCount").get(function () {
+    return this.reactions.length;
+})
+
+const reactionSchema = new Schema({
+    reactionId: {
+        type: Schema.Types.ObjectId,
+        default: () => new Types.ObjectId(),
+    },
+    reactionBody: {
+        type: String,
+        required: true,
+        max_length: 280,
+    },
+    username: {
+        type: String,
+        required: true,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now(),
+    }
+})
+
